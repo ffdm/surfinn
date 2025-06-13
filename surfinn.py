9bc3beddf77274613426b1bf42f604c1bb16f2d6
Finn Moore <fdm@umich.edu>
2025-06-13 16:44:56 -0400

    Most basic browser. Can read text from HTML pages.

---
+++ surfinn.py
@@ -0,0 +1,76 @@
+import socket
+import sys
+
+class URL:
+
+    def __init__(self, url):
+        self.scheme, url = url.split('://', 1)
+
+        # Browser only supports http
+        assert self.scheme == 'http'
+
+        if '/' not in url:
+            url += '/'
+        self.host, url = url.split('/', 1)
+        self.path = '/' + url
+    
+    def request(self):
+        # define socket
+        s = socket.socket(
+            family=socket.AF_INET,
+            type=socket.SOCK_STREAM,
+            proto=socket.IPPROTO_TCP
+        )
+
+        # define request
+        request = f'GET {self.path} HTTP/1.0\r\n'
+        request += f'Host: {self.host}\r\n'
+        request += '\r\n'
+
+        # connect to socket 
+        s.connect((self.host, 80))
+
+        # send request
+        s.send(request.encode('utf8'))
+
+        # receive response
+        response = s.makefile('r', encoding='utf8', newline='\r\n')
+
+        # close socket
+        s.close()
+
+        # read statusline
+        statusline = response.readline()
+        version, status, description = statusline.split(" ", 2)
+
+        # read headers
+        response_headers = {}
+        while (line := response.readline()) != '\r\n':
+            header, value = line.split(':', 1)
+            response_headers[header.lower()] = value.strip()
+       
+        # check for compressed data (not currently supported)
+        assert "transfer-encoding" not in response_headers
+        assert "content-encoding" not in response_headers
+
+        # read body
+        body = response.read()
+        return body
+
+def show(body):
+    # prints text without tags from html file
+    in_tag = False
+    for c in body:
+        if c == "<":
+            in_tag = True
+        elif c == ">":
+            in_tag = False
+        elif not in_tag:
+            print(c, end="")
+
+def load(url):
+    body = url.request()
+    show(body)
+
+if __name__ == "__main__":
+    load(URL(sys.argv[1]))
