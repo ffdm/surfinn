1f6999695ef20e3ac4a468bda3f71f2965ac6b0f
Finn Moore <fdm@umich.edu>
2025-07-30 15:24:39 -0400

    Add mouse wheel scroll, update README

--- README.md
+++ README.md
@@ -15,14 +15,13 @@ formatting.
 
 ## Setup
 
-Surfinn uses as the graphical toolkit. This forces me to implement
+Surfinn uses Tkinter as the graphical toolkit. This forces me to implement
 various features manually and learn more in the process.
 
 ---
 
 ## Todo:
 
-- [ ] Initial GUI
 - [ ] Add support for file scheme
 - [ ] Add support for data scheme
 - [ ] Add support for &lt; and &gt; entities
@@ -30,5 +29,15 @@ various features manually and learn more in the process.
 - [ ] Keep-alive to speed up repeated requests
 - [ ] Add caching
 - [ ] Support HTTP compression
+- [ ] Line breaks
+- [ ] Make browser resizeable
+- [ ] Add scrollbar
+- [ ] Add support for emojis (openmoji)
+- [ ] Add about:blank
+- [ ] Alternate text direction
+- [ ] Support mouse wheel scroll for other OS
+- [ ] Prevent user from scrolling down past page bottom
+- [x] Support mouse wheel scroll
+- [x] Initial GUI
 - [x] Support redirects
 
--- surfinn.py
+++ surfinn.py
@@ -99,10 +99,13 @@ class URL:
         s.close()
 
         return response
-        
+
 WIDTH, HEIGHT = 800, 600
+HSTEP, VSTEP = 13, 18
+SCROLL_STEP = 100
 
 class Browser:
+
     def __init__(self):
         self.window = tkinter.Tk()
         self.canvas = tkinter.Canvas(
@@ -111,6 +114,35 @@ class Browser:
             height=HEIGHT
         )
         self.canvas.pack()
+        self.scroll = 0
+        self.window.bind("<Up>", self.scrollup)
+        self.window.bind("<Down>", self.scrolldown)
+        self.window.bind("<MouseWheel>", self.mousescroll)
+
+    def mousescroll(self, event):
+        # MacOS 
+        self.scroll -= event.delta
+        if self.scroll < 0: self.scroll = 0
+        self.draw()
+
+    def scrolldown(self, event):
+        self.scroll += SCROLL_STEP
+        print(event.delta)
+        sys.exit()
+        self.draw()
+
+    def scrollup(self, event):
+        self.scroll -= SCROLL_STEP
+        # prevent scrolling above page top
+        if self.scroll < 0: self.scroll = 0
+        self.draw()
+
+    def draw(self):
+        self.canvas.delete("all")
+        for x, y, c in self.display_list:
+            if y > self.scroll + HEIGHT: continue
+            if y + VSTEP < self.scroll: continue
+            self.canvas.create_text(x, y - self.scroll, text=c)
 
     def load(self, url):
         response = url.request()
@@ -128,15 +160,20 @@ class Browser:
 
         # display html body
         text = lex(response.body)
-        HSTEP, VSTEP = 13, 18
-        cursor_x, cursor_y = HSTEP, VSTEP
-        for c in text:
-            self.canvas.create_text(cursor_x, cursor_y, text=c)
-            cursor_x += HSTEP
-            if cursor_x >= WIDTH - HSTEP:
-                cursor_y += VSTEP
-                cursor_x = HSTEP
+        self.display_list = layout(text)
+        self.draw()
 
+
+def layout(text):
+    display_list = []
+    cursor_x, cursor_y = HSTEP, VSTEP
+    for c in text:
+        display_list.append((cursor_x, cursor_y, c))
+        cursor_x += HSTEP
+        if cursor_x >= WIDTH - HSTEP:
+            cursor_y += VSTEP
+            cursor_x = HSTEP
+    return display_list
 
 def lex(body):
     # returns text without tags from html file
